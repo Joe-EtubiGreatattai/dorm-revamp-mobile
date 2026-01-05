5096b226206b180ac1af4895160123ca59667e07
Joe-EtubiGreatattai <greatattai442442@gmail.com>
2026-01-05 17:22:51 +0100

    chore: Update API URL to production

--- config/api.ts
+++ config/api.ts
@@ -1,19 +1,9 @@
-import Constants from 'expo-constants';
-import { Platform } from 'react-native';
 
-const PORT = '5001';
+// Production URL
+const PRODUCTION_URL = 'https://dorm-revamp-backend.onrender.com';
 
-const getLocalIP = () => {
-    const debuggerHost = Constants.expoConfig?.hostUri;
-    const localhost = Platform.OS === 'ios' ? 'localhost' : '10.0.2.2';
-    return debuggerHost ? debuggerHost.split(':')[0] : localhost;
-};
-
-const LOCAL_IP = getLocalIP();
-
-// Use environment variable or default to local IP
-export const API_URL = `http://${LOCAL_IP}:${PORT}/api`;
-export const SOCKET_URL = `http://${LOCAL_IP}:${PORT}`;
+export const API_URL = `${PRODUCTION_URL}/api`;
+export const SOCKET_URL = PRODUCTION_URL;
 export const PAYSTACK_PUBLIC_KEY = 'pk_test_a15103affe18985815a036f9092bc2ffe442b3d5';
 
 console.log('API_URL Configured:', API_URL);
